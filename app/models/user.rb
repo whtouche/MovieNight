c497122feecfdbc67a9f8f870ec6b9d97ee98862
Ben White <whtouche@gmail.com>
2014-07-12 16:30:39 -0400

    Create HABTM join table for events / users
    *Soon I will figure out how this works!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  has_many :articles, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  has_and_belongs_to_many :events, dependent: :destroy
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707203253) do
+ActiveRecord::Schema.define(version: 20140712202206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20140707203253) do
     t.time     "time"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "events_users", id: false, force: true do |t|
+    t.integer "event_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
