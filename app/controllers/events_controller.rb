4893c127bbb1dc5640b682b3029359701f2d1f50
Ben White <whtouche@gmail.com>
2014-07-13 02:10:21 -0400

    User can create event based on API query data

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,25 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    #magic
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event), notice: "Event Created Successfully"
+    else
+      render :new, notice: "Failure!"
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :event_description, :movie, :theatre,
+      :date)
+  end
 end
--- app/models/showtime.rb
+++
@@ -1,9 +0,0 @@
-class Showtime
-  # Needed if class doesn't inherit from ActiveRecord::Base
-  extend ActiveModel::Naming
-  include ActiveModel::Conversion
-  def persisted?
-    false
-  end
-  attr_accessor :movie, :datetime
-end
