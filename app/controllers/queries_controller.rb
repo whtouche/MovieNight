efa5ce2db15beb5b5382fef63544065799a3af95
Ben White <whtouche@gmail.com>
2014-07-13 01:26:21 -0400

    Updated API query methods and corresponding views

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -2,34 +2,52 @@ class QueriesController < ApplicationController
   TMS = TMSAPI::API.new :api_key => ENV['TMS_TOKEN']
 
   def new
-    @query = Query.new
-
+    # Delete this? Since not persisting
+    #@query = Query.new
   end
 
   def create
-    @query = Query.new
-    @query.zip = query_params['zip']
-    @query.date = query_params['date']
-    @results = data(@query.zip, @query.date)
+    #@query = Query.new
+    @zip = params['zip']
+    @date = params['date']
+    @results = data(@zip, @date)
 
-    binding.pry
+    #binding.pry
+  end
+
+  def show
+    result = data(params[:zip], params[:date])
+    @result = result.select { |movie| movie.title == params[:title] }.first
+    @event = Event.new
   end
 
   private
 
-  def query_params
-    params.require(:query).permit(:zip, :date)
-  end
+  #def query_params
+  #  params.require(:query).permit(:zip, :date)
+  #end
 
   def data(zip, date) # Uses TMS API with TMS gem and returns data for all movies playing in area by date
     TMS.movies.theatres.showings(zip: "#{zip}", startDate: "#{date}")
   end
 
-  def movies(data) # Get a list of all movie titles ##DOESN'T WORK
+  def movies(data)
     @movies = []
     data.each do |movie|
       movies << @movie.title
     end
     return movies
   end
+
+  def save_data_from_api(data)
+    data.each do |movie|
+      @movie = Movie.new
+      @movie.title = movie.title
+      @movie.showtimes << movie.showtimes
+
+    end
+# Create movie model
+# Showtimes column = array
+  end
+
 end
