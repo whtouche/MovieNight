39cb922962c4163ecc0b4658b792fb407d60e410
Ben White <whtouche@gmail.com>
2014-07-13 02:21:50 -0400

    Set up relationships between users, events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  has_and_belongs_to_many :events, dependent: :destroy
-
+  has_many :events, dependent: :destroy
+  has_many :rsvps
 end
