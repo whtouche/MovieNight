735c2dcc46f067a1e67e7fea2b21b30a9995421a
Ben White <whtouche@gmail.com>
2014-07-13 01:27:33 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :showtimes, only: [:index]
   resources :events
   resources :queries #, only: [:create, :new, :show]
+  get "/results" => "queries#show"
 
   #get 'browse', to: 'queries#new'
   root 'home#show'
