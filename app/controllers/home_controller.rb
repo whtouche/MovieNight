53cf2317b44bb08ff6a04ea6723718f392e22eee
Ben White <whtouche@gmail.com>
2014-09-10 21:19:02 -0400

    Misc. changes since I remembered I was using git

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def show
     @events = Event.all # This isn't right, should show all events you have joined (or started [BUT HOW?])
+    render layout: "full"
   end
 end
